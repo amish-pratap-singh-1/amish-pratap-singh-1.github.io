89ffda2eccf48267706366f20a083e6b5d7977e0
amish-pratap-singh-1 <amishpratapsingh13@gmail.com>
2026-04-06 14:19:22 +0530

    Updates

---
+++ _next/static/KhjVkdSh3H4de5-ZitAlg/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/0_ufdx_bi~q.7.js"
+  ],
+  "/_error": [
+    "static/chunks/0j7hhb-pr_a5l.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
